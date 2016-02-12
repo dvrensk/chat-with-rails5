da3653acfab07009781819cd524bfbf05f6567a1
David Vrensk <david@vrensk.com>
2016-02-13 00:04:39 +0100

    use same view for live and history

--- app/jobs/message_broadcast_job.rb
+++ app/jobs/message_broadcast_job.rb
@@ -2,6 +2,14 @@ class MessageBroadcastJob < ApplicationJob
   queue_as :default
 
   def perform(message)
-    ActionCable.server.broadcast "room_channel", message: message.content
+    ActionCable.server.broadcast "room_channel", message: render(message)
+  end
+
+  private \
+  def render(message)
+    ApplicationController.renderer.render(
+      partial: "messages/message",
+      locals: {message: message},
+    )
   end
 end
